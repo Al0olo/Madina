399915add47f53edfa07988a4f35a083ed896718
ali <straoid@gmail.com>
2024-05-11 12:56:24 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Madina',
+  webDir: 'dist'
+};
+
+export default config;
