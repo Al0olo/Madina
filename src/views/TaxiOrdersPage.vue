5f5bae579ca09c6bb6beecdca31644268c5ea810
ali <straoid@gmail.com>
2024-05-12 12:11:02 +0300

    add additional pages

--- src/views/TaxiOrdersPage.vue
+++ src/views/TaxiOrdersPage.vue
@@ -4,24 +4,23 @@
   
       <ion-content :fullscreen="true">
         <div id="container">
-            <ion-searchbar placeholder="Search"></ion-searchbar>
             <ion-card id="taxi-banner">
                 <ion-card-content>
                     <img alt="Silhouette of mountains" src="../assets/img/InsideCityBanner.svg" style="width:100%;height:100%;position:fixed;top:0;left:0" />
-                  <ion-button id="login-button">{{ ("Order Taxi") }} </ion-button>
+                    <ion-nav-link router-direction="forward" :component="insideCity"> <ion-button id="login-button">{{ ("Order Taxi") }} </ion-button></ion-nav-link>
                 </ion-card-content>
             </ion-card>
             <ion-card id="taxi-banner">
                 <ion-card-content>
                     <img alt="Silhouette of mountains" src="../assets/img/OutsideCityBanner.svg" style="width:100%;height:100%;position:fixed;top:0;left:0" />
-                  <ion-button id="login-button">{{ ("Order Taxi") }} </ion-button>
+                    <ion-nav-link router-direction="forward" :component="OutsideCity"> <ion-button id="login-button">{{ ("Order Taxi") }} </ion-button></ion-nav-link>
                 </ion-card-content>
             </ion-card>
             
             <ion-card id="airports-banner">
                 <ion-card-content>
                     <img alt="Silhouette of mountains" src="../assets/img/BetweenGovernatesBanner.svg" style="width:100%;height:100%;position:fixed;top:0;left:0" />
-                  <ion-button id="login-button">{{ ("Order Taxi") }} </ion-button>
+                    <ion-nav-link router-direction="forward" :component="Airports"> <ion-button id="login-button">{{ ("Order Taxi") }} </ion-button></ion-nav-link>
                 </ion-card-content>
             </ion-card>
         </div>
@@ -30,14 +29,20 @@
   </template>
   
 <script lang="ts">
-//   import my from './HomePage.vue';
-  import { IonContent,  IonPage,IonCard, IonCardContent, IonSearchbar,IonButton, IonNavLink } from '@ionic/vue';
+  import insideCity from './InsideCityPage.vue';
+  import OutsideCity from './OutsideCityPage.vue';
+  import Airports from './AirportsPage.vue';
+
+
+  import { IonContent,  IonPage,IonCard, IonCardContent,IonButton, IonNavLink } from '@ionic/vue';
   
   export default {
-      components: { IonContent, IonPage,IonCard,IonCardContent, IonSearchbar,IonButton, IonNavLink},
+      components: { IonContent, IonPage,IonCard,IonCardContent,IonButton, IonNavLink},
       data() {
         return {
-            // lo: my,
+          insideCity: insideCity,
+          OutsideCity: OutsideCity,
+          Airports: Airports,
         };
       },
     };
