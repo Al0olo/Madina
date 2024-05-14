a0c65b32a973fb917a30c1c7ab9719cd78aabad0
ali <straoid@gmail.com>
2024-05-14 07:54:28 +0300

    create axios message

---
+++ src/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+});
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => error
+);
+
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
